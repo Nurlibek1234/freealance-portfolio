5a1f8d4b124e20230efe6c874f6d39f9ec07f43e
Nurlibek <119851276+Nurlibek1234@users.noreply.github.com>
2023-02-11 17:02:56 +0500

    upload some files | modules

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import "./style/main.css";
+
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+
+import Navbar from "./components/navbar/Navbar";
+import Footer from "./components/footer/Footer";
+import Home from "./pages/Home";
+import Projects from "../src/pages/Projects";
+import Project from "./pages/Project";
+import Contacts from "./pages/Contact";
+import ScrollToTop from "./utils/scrollToTop";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Navbar />
+        <ScrollToTop />
+
+        <Switch>
+          <Route path="/project/:id">
+            <Project />
+          </Route>
+          <Route path="/projects">
+            <Projects />
+          </Route>
+          <Route path="/contacts">
+            <Contacts />
+          </Route>
+          <Route path="/" exact>
+            <Home />
+          </Route>
+        </Switch>
+
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/components/footer/Footer.js
@@ -0,0 +1,29 @@
+import "./style.css";
+
+import vk from "./../../img/icons/vk.svg"
+import linkedin from "./../../img/icons/linkedIn.svg"
+import instagram from "./../../img/icons/instagram.svg"
+import twitter from "./../../img/icons/twitter.svg"
+
+
+const Footer = () => {
+    return ( 
+         <footer className="footer">
+    <div className="container">
+        <div className="footer__wrapper">
+            <ul className="social">
+                <li className="social__item"><a href="#!"><img src={vk} alt="Link" /></a></li>
+                <li className="social__item"><a href="#!"><img src={instagram} alt="Link" /></a></li>
+                <li className="social__item"><a href="#!"><img src={twitter} alt="Link" /></a></li>
+                <li className="social__item"><a href="#!"><img src={linkedin} alt="Link" /></a></li>
+            </ul>
+            <div className="copyright">
+                <p>© 2023 frontend-dev.com</p>
+            </div>
+        </div>
+    </div>
+</footer>
+ )
+}
+ 
+export default Footer;
